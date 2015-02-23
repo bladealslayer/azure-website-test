c401106e1865fdad1ce7bb52630cd85803cec0bf
Boyan Tabakov <boyan.tabakov@futurice.com>
2015-02-23 11:55:49 +0200

    test vnet access

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-echo "btab-test-website\n"
+echo "btab-test-website\n";
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, 'http://10.123.1.5');
+$resp = curl_exec($curl);
+
+echo $resp;
 
 ?>
