5b353889c243573a3e0ecbb2b991d95de7889519
Boyan Tabakov <boyan.tabakov@futurice.com>
2015-02-23 13:01:24 +0200

    other private ip

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-echo "btab-test-website\n";
+echo "btab-test-website<br/>";
 $curl = curl_init();
 
+echo "10.123.1.4:<br/>";
+curl_setopt($curl, CURLOPT_URL, 'http://10.123.1.4');
+$resp = curl_exec($curl);
+
+echo $resp;
+echo "10.123.1.5:<br/>";
 curl_setopt($curl, CURLOPT_URL, 'http://10.123.1.5');
 $resp = curl_exec($curl);
 
